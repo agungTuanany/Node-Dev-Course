8f69c110f932def2be7af41fb4c82b8f2785dd16
agungTuanany <agung.tuanany@gmail.com>
2018-04-25 12:12:49 +0700

    quick-tip format printing object with JSON.stringify()

--- 02.Aysnc-NodeJS-Weather-App/weather-app/app.js
+++ 02.Aysnc-NodeJS-Weather-App/weather-app/app.js
@@ -1,5 +1,12 @@
+/**
 // https://maps.google.com/maps/api/geocode/json
 
+@param JSON.stringify(body,undifined,3) there is three argument we pass inspect
+the last argument form JSON.stringify() is a number
+the number is specified indentation in JSON format that we fetch.
+
+
+**/
 const request = require('request');
 const url = ' https://maps.google.com/maps/api/geocode/json?address=%20Kampus%20upiYAI%20salemba%20raya%20jakarta%20pusat ';
 const url2 = ' https://maps.google.com/maps/api/geocode/json?address=%20Pasar%20Kenari%20Salemba%20Raya%20Jakarta%20Pusat ';
@@ -9,5 +16,5 @@ request({
     // url: url2,
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    console.log(JSON.stringify(body, undefined, 3));
 });
