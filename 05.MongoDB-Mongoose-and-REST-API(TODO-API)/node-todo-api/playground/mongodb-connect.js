75111d7d1ed00e20f4909cc48a37a5bab2925f9b
agungTuanany <agung.tuanany@gmail.com>
2018-04-29 07:35:05 +0700

    Mongo and Writing Data 01.insertOne with mongo v3.x.x

--- 05.MongoDB-Mongoose-and-REST-API(TODO-API)/node-todo-api/playground/mongodb-connect.js
+++ 05.MongoDB-Mongoose-and-REST-API(TODO-API)/node-todo-api/playground/mongodb-connect.js
@@ -2,14 +2,44 @@
 1. Make folder directory mongo-data
 2. connect mongod on terminal with to mongo-data path
     mongod --dbpath ~/yourpath/mongo-data
+3. run your node.
+
+@param db.collection() is argument to insert into a new record.
+@param (result.ops) , the ops attribut is gonna store all of the docs we inserted @param InsertOne()
 */
 const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
-MongoClient.connect('mongodb://localhost:27017/Todoapp', (err, db) => {
+const url = 'mongodb://localhost:27017'
+
+const dbName = 'TodoApp'
+
+MongoClient.connect(url, (err, client) => {
     if (err) {
-        return console.loog('Unable to connect to MongoDB server');
+        return console.log('Unable to connect to MongoDB server');
     }
     console.log('Connected to MongoDB server');
 
-    db.close();
+    const db = client.db(dbName);
+
+    const insertData1 =[ {
+        text: 'Something to do',
+        completed: false
+    }, {
+        text: "Something to do 2",
+    }];
+
+    const insertData2 = {
+        text: 'someting to do from insertData2 data=1',
+        completed: false
+    };
+
+    db.collection('Todo').insertOne(insertData2, (err, result) => {
+        if (err) {
+            assert.equal(null, err);
+            return(err);
+        }
+        assert.equal(1, result.insertedCount);
+        console.log(JSON.stringify(result.ops, undefined, 3));
+    });
 });
