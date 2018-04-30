d7a3653c5ad4253f65342d2b6098b569bf623a7d
agungTuanany <agung.tuanany@gmail.com>
2018-04-30 15:15:52 +0700

    Mongoose Queries and ID Validation 02.ID Validation

--- 05.MongoDB-Mongoose-and-REST-API(TODO-API)/node-todo-api/playground/mongoose-queries.js
+++ 05.MongoDB-Mongoose-and-REST-API(TODO-API)/node-todo-api/playground/mongoose-queries.js
@@ -1,19 +1,29 @@
+/**
+@param ObjectID.isValid() : is a function that provide from mongoose (objectId)
+*/
+const {ObjectID} = require('mongodb');
+
 const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
 
-let id = "5ae6c47a589dd2319c303a62";
+let id = "5ae6c47a589dd2319c303a6";//2
 
-Todo.find({
-    _id: id
-}).then((todos) => {
-    console.log('Todos', todos);
-});
+if(!ObjectID.isValid(id)) {
+    console.log("Id is not valid");
+}
+
+// Todo.find({
+//     _id: id
+// }).then((todos) => {
+//     console.log('Todos', todos);
+// });
+
 // if you wanna find one Document by something other then _id recomend using findOne
-Todo.findOne({
-    _id: id
-}).then((todo) => {
-    console.log("Todo", todo);
-});
+// Todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     console.log("Todo", todo);
+// });
 
 // if you wanna find one Document by 'id' recomend using findById
 Todo.findById(id).then((todo) => {
@@ -21,5 +31,5 @@ Todo.findById(id).then((todo) => {
         return console.log('Id not found');
     }
     console.log("Todo By Id", todo);
-});
+}).catch((e) => console.log(e));
 
--- 05.MongoDB-Mongoose-and-REST-API(TODO-API)/node-todo-api/server/tests/server.test.js
+++ 05.MongoDB-Mongoose-and-REST-API(TODO-API)/node-todo-api/server/tests/server.test.js
@@ -1,7 +1,11 @@
 /**
-@param beforeEach : is a function 'mocha' provide it. Is  gonna let us run some code before every single test case, we gonna use beforeEach to setup database in the way useful, for now all it's to make sure database is empty
+@param beforeEach : is a function 'mocha' provide it. Is gonna let us run some code before every single test case,
+ we gonna use beforeEach to setup database in the way useful,
+ for now all it's to make sure database is empty
 
-This function gonna run before all every test cases and it's only gonna move onto the test cases once we call 'done()' , Which mean we can do asynchronous inside of it
+This function gonna run before all every test cases
+ and it's only gonna move onto the test cases once we call 'done()' ,
+ Which mean we can do asynchronous inside of it
 
 @param Todo.remove({}) the side effect using 'beforeEach' in this case is the data in document collection all wipe out.
 **/
