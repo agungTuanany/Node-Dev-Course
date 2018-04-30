331b37ee07dddd59277e002f7b3785681cfb7014
agungTuanany <agung.tuanany@gmail.com>
2018-05-01 06:59:40 +0700

    Setup app for heroku

--- 05.MongoDB-Mongoose-and-REST-API(TODO-API)/node-todo-api/server/server.js
+++ 05.MongoDB-Mongoose-and-REST-API(TODO-API)/node-todo-api/server/server.js
@@ -7,7 +7,7 @@ const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -50,7 +50,7 @@ app.get('/todos/:id', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Started on port ${port} `);
+    console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
