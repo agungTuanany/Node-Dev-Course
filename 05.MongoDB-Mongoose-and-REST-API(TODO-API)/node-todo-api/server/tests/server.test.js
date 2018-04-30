f5dfcf953e3de33ed33fab9054f5de24da2da856
agungTuanany <agung.tuanany@gmail.com>
2018-04-30 19:33:05 +0700

    Add test cases for GET /todes/:id

--- 05.MongoDB-Mongoose-and-REST-API(TODO-API)/node-todo-api/server/tests/server.test.js
+++ 05.MongoDB-Mongoose-and-REST-API(TODO-API)/node-todo-api/server/tests/server.test.js
@@ -1,5 +1,6 @@
 /**
-@param beforeEach : is a function 'mocha' provide it. Is gonna let us run some code before every single test case,
+@param beforeEach : is a function 'mocha' provide it.
+ Is gonna let us run some code before every single test case,
  we gonna use beforeEach to setup database in the way useful,
  for now all it's to make sure database is empty
 
@@ -8,17 +9,23 @@ This function gonna run before all every test cases
  Which mean we can do asynchronous inside of it
 
 @param Todo.remove({}) the side effect using 'beforeEach' in this case is the data in document collection all wipe out.
+
+@param .toHexString() converting '_id' into a String because we gonna pass in into URL,
+because '_id' is an object, so we can start make some assertions about what should happen when this request get fired.
 **/
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 // add dummy todos
 const todos = [{
+    _id: new ObjectID(),
     text: "First test todo with mocha, expect and superTest"
 }, {
+    _id: new ObjectID(),
     text: "Second test todo with expect, mocha and superTest"
 }];
 
@@ -83,6 +90,34 @@ describe('GET /todos', () => {
             .expect((res) => {
                 expect(res.body.todos.length).toBe(2);
             })
+            .end(done);
+    });
+});
+
+describe('GET /todos/:id', () => {
+    it('should return todo document', (done) => {
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(todos[0].text);
+            })
+            .end(done);
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        let hexId = new ObjectID().toHexString();
+        request(app)
+            .get(`/todos/${hexId}`)
+            .expect(404)
+            .end(done);
+
+    });
+
+    it('should return 404 if for non-object ids', (done) => {
+        request(app)
+            .get(`/todos/123abc`)
+            .expect(404)
             .end(done);
     });
 });
