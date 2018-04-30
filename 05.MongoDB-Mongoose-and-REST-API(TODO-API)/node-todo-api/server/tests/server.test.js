d7a69e0ca508f85b236c121d4f26f27db37e2a0f
agungTuanany <agung.tuanany@gmail.com>
2018-04-30 12:19:21 +0700

    Testing Post_todos 02.Adding testing lifecycle method

--- 05.MongoDB-Mongoose-and-REST-API(TODO-API)/node-todo-api/server/tests/server.test.js
+++ 05.MongoDB-Mongoose-and-REST-API(TODO-API)/node-todo-api/server/tests/server.test.js
@@ -1,8 +1,20 @@
+/**
+@param beforeEach : is a function 'mocha' provide it. Is  gonna let us run some code before every single test case, we gonna use beforeEach to setup database in the way useful, for now all it's to make sure database is empty
+
+This function gonna run before all every test cases and it's only gonna move onto the test cases once we call 'done()' , Which mean we can do asynchronous inside of it
+
+@param Todo.remove({}) the side effect using 'beforeEach' in this case is the data in document collection all wipe out.
+**/
 const expect = require('expect');
 const request = require('supertest');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
+
+// adding testing lifecycle method
+beforeEach((done) => {
+    Todo.remove({}).then(() => done());
+})
 
 describe('POST /todos', () => {
     it('should create a new todo', (done) => {
