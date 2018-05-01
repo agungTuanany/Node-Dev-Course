089264dc57d356fa8498d11873fb01e62c13781c
agungTuanany <agung.tuanany@gmail.com>
2018-05-01 20:01:56 +0700

    Security and Authentication 01.Setting Up The User Model

--- 05.MongoDB-Mongoose-and-REST-API(TODO-API)/node-todo-api/server/models/user.js
+++ 05.MongoDB-Mongoose-and-REST-API(TODO-API)/node-todo-api/server/models/user.js
@@ -1,12 +1,35 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const User = mongoose.model('users', {
     email: {
-        require: true,
-        trim:true,
         type: String,
-        minLength:1
-    }
+        required: true,
+        trim:true,
+        minLength:1,
+        unique: true,
+        validate: {
+            validator: (value) => {
+                return validator.isEmail(value);
+            },
+            message: `{VALUE} is not valid email`
+        }
+    },
+    password: {
+        type: String,
+        require: true,
+        minLength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
 
 module.exports = {
