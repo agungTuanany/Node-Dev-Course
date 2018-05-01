099d18263614728256731a0bec05b085894dfd8e
agungTuanany <agung.tuanany@gmail.com>
2018-05-02 01:13:03 +0700

    Security and Auth 04.Explaining JWT

--- 05.MongoDB-Mongoose-and-REST-API(TODO-API)/node-todo-api/playground/hashing.js
+++ 05.MongoDB-Mongoose-and-REST-API(TODO-API)/node-todo-api/playground/hashing.js
@@ -1,27 +1,38 @@
 const {SHA256} = require('crypto-js');
-
-var message = "I am user number 3";
-var hash = SHA256(message).toString();
-
-console.log(`Message: ${message}`);
-console.log(`Hash: ${hash}`);
+const jwt = require('jsonwebtoken');
 
 var data = {
-    id: 4
-};
-var token = {
-    data,
-    hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+    id: 10
 }
 
-token.data.id = 5;
-token.hash = SHA256(JSON.stringify(token.data)).toString();
+var token = jwt.sign(data, '4321ytrewq');
+console.log(token);
+
+var decoded = jwt.verify(token, "4321ytrewq");
+console.log('decoded', decoded);
+
+// var message = "I am user number 3";
+// var hash = SHA256(message).toString();
+
+// console.log(`Message: ${message}`);
+// console.log(`Hash: ${hash}`);
+
+// var data = {
+//     id: 4
+// };
+// var token = {
+//     data,
+//     hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+// }
+
+// token.data.id = 5;
+// token.hash = SHA256(JSON.stringify(token.data)).toString();
 
 
-var resultHash = SHA256(JSON.stringify(token.data) + 'somesceter').toString();
+// var resultHash = SHA256(JSON.stringify(token.data) + 'somesceter').toString();
 
-if (resultHash === token.hash) {
-    console.log('Data was not changed');
-} else {
-    console.log('Data was changed Do not trust! ');
-}
+// if (resultHash === token.hash) {
+//     console.log('Data was not changed');
+// } else {
+//     console.log('Data was changed Do not trust! ');
+// }
