c7743fbaeb13e883c193aaae41a01e4aa7410fa3
agungTuanany <agung.tuanany@gmail.com>
2018-05-06 16:47:39 +0700

    Storing Users with ES6 Classes 03.Completed method and completed Test users

--- 06.Real-Time-Web-app-with-Socket-io/node-chat-app/server/utils/users.js
+++ 06.Real-Time-Web-app-with-Socket-io/node-chat-app/server/utils/users.js
@@ -3,11 +3,29 @@ class Users {
         this.users = [];
     }
     addUser (id, name, room) {
-        var user = {id, name, room};
+        let user = {id, name, room};
         this.users.push(user);
         return user;
     }
-}
+    removeUser (id) {
+        let user = this.getUser(id);
+
+        if (user) {
+            this.users = this.users.filter((user) => user.id !== id);
+        }
+
+        return user;
+    }
+    getUser (id) {
+        return this.users.filter((user) => user.id === id)[0];
+    }
+    getUserList (room) {
+        let users = this.users.filter((user) => user.room === room);
+        let namesArray = users.map((user) => user.name);
+
+        return namesArray;
+    }
+};
 
 module.exports = {Users};
 
@@ -19,9 +37,9 @@ module.exports = {Users};
     // romm: "The Office Fans"
 // }]
 
-// addUser(id, name, romm)
-// removeUser(id)
-// getuser(id) -> method
+// addUser(id, name, romm) -> method
+// removeUser(id) -> method
+// getUser(id) -> method
 // getUserList(room) -> -method
 
 
--- 06.Real-Time-Web-app-with-Socket-io/node-chat-app/server/utils/users.test.js
+++ 06.Real-Time-Web-app-with-Socket-io/node-chat-app/server/utils/users.test.js
@@ -3,16 +3,78 @@ const expect = require('expect');
 const {Users} = require('./users');
 
 describe('Users', () => {
+    var users;
+
+    // to initialize some data before all singgle test was called
+    beforeEach(() => {
+        users = new Users();
+        users.users = [{
+            id: '1',
+            name: 'Jono',
+            room: 'Node Course'
+        }, {
+            id: '2',
+            name: 'Tiancha',
+            room: 'React Course'
+        }, {
+            id: '1',
+            name: 'Jaroko',
+            room: 'Node Course'
+        }];
+    });
+
     it('should add new user', () => {
-        var users = new Users();
-        var user = {
+        let users = new Users();
+        let user = {
             id: 'psdajo32133ff',
             name: 'Jono',
             room: 'The Office Fans'
         };
-        var resUser = users.addUser(user.id, user.name, user.room);
+        let resUser = users.addUser(user.id, user.name, user.room);
 
         // the 1st users: belong to users variable, the 2nd users belong to this.users[];
         expect(users.users).toEqual([user]);
     });
-})
+
+    it('should remove a user', () => {
+        let userId = '2';
+        let user = users.removeUser(userId);
+
+        expect(user.id).toBe(userId);
+        expect(users.users.length).toBe(2);
+    });
+
+    it('should not remove user', () => {
+        let userId = '99';
+        let user = users.removeUser(userId);
+
+        expect(user).toBeFalsy();
+        expect(users.users.length).toBe(3);
+    });
+
+    it('should find user by id', () => {
+        let userId = "2";
+        let user = users.getUser(userId);
+
+        expect(user.id).toBe(userId);
+    });
+
+    it('should not find user by id', () => {
+        let userId = '99';
+        var user = users.getUser(userId);
+
+        expect(user).toBeFalsy();
+    });
+
+    it('should return names for node course,', () => {
+        let userList = users.getUserList('Node Course');
+
+        expect(userList).toEqual(['Jono', 'Jaroko']);
+    });
+
+    it('should return name for react course', () => {
+        let userList = users.getUserList('React Course');
+
+        expect(userList).toEqual(['Tiancha']);
+    })
+});
